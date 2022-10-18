5e15a3e754740ccb4a44373ed40ad1d933a1fb73
beomkim <k1b219@naver.com>
2022-10-18 12:16:41 +0900

    [initial commit]

---
+++ src/main/java/com/org/mamoru/service/KafkaConsumer.java
@@ -0,0 +1,17 @@
+package com.org.mamoru.service;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOError;
+import java.io.IOException;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics="test", groupId = ConsumerConfig.GROUP_ID_CONFIG)
+    public void consume(String message) throws IOException {
+        System.out.println("Kafka Consumed message :" + message);
+    }
+}
